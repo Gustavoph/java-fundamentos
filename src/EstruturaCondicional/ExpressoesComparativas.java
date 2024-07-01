2d0af602cea233febae48eb297c62a3ad02d6fc9
Gustavo Oliveira <gusta.coliveira07@gmail.com>
2024-07-01 16:56:40 -0300

    feat: expressoes comparativas

---
+++ src/EstruturaCondicional/ExpressoesComparativas.java
@@ -0,0 +1,15 @@
+package EstruturaCondicional;
+
+public class ExpressoesComparativas {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 5;
+
+    System.out.printf("X é igual a Y: %b %n", x == y);
+    System.out.printf("X é maior a Y: %b %n", x > y);
+    System.out.printf("X é maior ou igual a Y: %b %n", x >= y);
+    System.out.printf("X é menor a Y: %b %n", x < y);
+    System.out.printf("X é menor ou igual a Y: %b %n", x <= y);
+    System.out.printf("X é diferente a Y: %b %n", x != y);
+  }
+}
