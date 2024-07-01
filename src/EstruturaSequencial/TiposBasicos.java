8553cfb2956bca37fa1f1b79e74fcc255247e5f6
Gustavo Oliveira <gusta.coliveira07@gmail.com>
2024-07-01 10:16:34 -0300

    feat: tipos basicos

---
+++ src/EstruturaSequencial/TiposBasicos.java
@@ -0,0 +1,24 @@
+package EstruturaSequencial;
+
+public class TiposBasicos {
+  public static void main(String[] args) {
+    // Para armazenar valores inteiros
+    byte varByte = 25;
+    short varShort = 1414;
+    int varInt = 2024;
+    long varLong = 1000000000;
+
+    // Para armazenar valores com ponto flutuante
+    float varFloat = 10.0f;
+    double varDouble = 123.50;
+
+    // Para armazenar caracteres unicode
+    char varChar = '\u0000';
+
+    // Para armazenar valores booleanos
+    boolean varBoolean = true;
+
+    // Para armazenar strings (Não é um tipo primitivo)
+    String varString = "Gustavo";
+  }
+}
