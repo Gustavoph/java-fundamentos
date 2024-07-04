db73c9f14f1a67497ae71f03dd678a9a68b0899d
gustavoph <gusta.coliveira07@gmail.com>
2024-07-04 10:54:07 -0300

    feat: exceptions e try/catch

---
+++ src/TratamentoExcecoes/Main.java
@@ -0,0 +1,25 @@
+package TratamentoExcecoes;
+
+/*
+ * Exceções
+ * - Uma exceção é qualquer condição de erro ou comportamento
+ * inesperado encontrado por um programa em execução
+ * 
+ * - Em Java, uma exceção é um objeto herdado da classe:
+ *  - java.lang.Exception - o compilador obriga a tratar ou propagar
+ *  - java.lang.RuntimeException - o compilador não obriga a tratar ou propagar
+ * 
+ * - Quando lançada, uma exceção é propagada na prilah de chamadas de métodos em
+ * execução, até que seja capturada (tratada) ou o programa seja encerrado
+ * 
+ * Error vs Exceptions
+ * - Erros são coisas que o programador não controla, como erro na maquina
+ * virtual do java ou estourar a memória, caso isso aconteça não tem o que o
+ * programador faça
+ * 
+ * - Já a exceção pode e deve ser tratada pelo programador
+ */
+
+public class Main {
+  
+}
